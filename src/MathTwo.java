0861d9f342a27e2bd36bb7aa78b76918e2c3e153
PerryP1 <perryapublico@gmail.com>
2018-07-12 11:26:38 -0400

    Initial commit

---
+++ src/MathTwo.java
@@ -0,0 +1,17 @@
+public class MathTwo {
+
+    public static void main (String [] args) {
+
+
+        int mynumber = 2;
+        double myothernumber = 1.7938;
+        System.out.println ("My number is " + mynumber);
+        System.out.println ("My number is " + myothernumber);
+    }
+// Write an application that assigns 2 to a variable named mynumber and 1.7938 to a variable named myothernumber, and then prints out the values to the console.
+    //*
+
+
+
+
+}
